9cb155179d2a3579cccff6c1cbfcd88db5b1cfb9
Owen Yoshishige <owenyoshishige@gmail.com>
2022-02-22 22:55:17 -0700

    make my first update

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -3,6 +3,61 @@
 
 // but you don't so you're going to write it from scratch:
 
+//input: mixed
+//output: string
+
 var stringifyJSON = function(obj) {
-  // your code goes here
+  console.log(obj);
+  if (typeof obj === 'string') {
+    return '"' + obj + '"';
+  } else if (typeof obj === 'number' || obj === null) {
+    return obj + '';
+  } else if (typeof obj === 'boolean') {
+    return obj + '';
+  }
+
+  var stringElements = function (element) {
+    if (Array.isArray(element) && element.length === 0) {
+      return '[]';
+    }
+
+    if (Array.isArray(element)) {
+      return '[' + element.map(stringElements) + ']';
+    }
+
+    if (typeof element === 'number') {
+      return element;
+    } else if (typeof element === 'string') {
+      console.log(`"${element}"`);
+      return `"${element}"`;
+    }
+  };
+
+  if (Array.isArray(obj)) {
+    return '[' + obj.map(stringElements).toString() + ']';
+  }
+
+  if (!Array.isArray(obj) && Object.keys(obj).length === 0) {
+    return '{}';
+  }
+
+  var processObj = function(object) {
+    for (var key in object) {
+      if (typeof object[key] === 'boolean' || object[key] === null) {
+        result += '"' + key + '":' + object[key] + ',';
+      } else if (typeof object[key] === 'object') {
+        processObj(object[key]);
+      } else {
+        result += '"' + key + '"' + ':' + '"' + object[key] + '",';
+      }
+    }
+  };
+
+  if (!Array.isArray(obj)) {
+    var result = '{';
+    processObj(obj);
+    result = result.substring(0, result.length - 1);
+    return result + '}';
+  }
+
 };
